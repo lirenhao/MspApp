6ca8b3a61fb5bc50d372978d9efb214e3fb53d93
lirenhao <renhao.li@bjyada.com>
2020-05-19 15:31:51 +0800

    密码修改校验

--- src/pages/modifyPwd/index.tsx
+++ src/pages/modifyPwd/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Form, Input, Button, notification } from 'antd';
+import { Card, Form, Input, Button, notification } from 'antd';
 import { LockOutlined } from '@ant-design/icons';
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import { ModifyData } from './data';
@@ -8,13 +8,14 @@ import { modifyPwd } from './service';
 
 const layout = {
   labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
+  wrapperCol: { span: 8 },
 };
 const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
 const ModifyView: React.FC<{}> = () => {
+  const [form] = Form.useForm();
 
   const handleSubmit = async (values: ModifyData) => {
     try {
@@ -31,58 +32,68 @@ const ModifyView: React.FC<{}> = () => {
 
   return (
     <PageHeaderWrapper>
-      <Form {...layout} onFinish={values => handleSubmit(values as ModifyData)}>
-        <Form.Item
-          name="oldPwd"
-          label={formatMessage({ id: 'modify.oldPwd.label' })}
-          rules={[
-            {
-              required: true,
-              message: formatMessage({ id: 'modify.oldPwd.role-required' }),
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder={formatMessage({ id: 'modify.oldPwd.placeholder' })}
-            prefix={<LockOutlined />}
-          />
-        </Form.Item>
-        <Form.Item
-          name="newPwd"
-          label={formatMessage({ id: 'modify.newPwd.label' })}
-          rules={[
-            {
-              required: true,
-              message: formatMessage({ id: 'modify.newPwd.role-required' }),
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder={formatMessage({ id: 'modify.newPwd.placeholder' })}
-            prefix={<LockOutlined />}
-          />
-        </Form.Item>
-        <Form.Item
-          name="checkPwd"
-          label={formatMessage({ id: 'modify.checkPwd.label' })}
-          rules={[
-            {
-              required: true,
-              message: formatMessage({ id: 'modify.checkPwd.role-required' }),
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder={formatMessage({ id: 'modify.checkPwd.placeholder' })}
-            prefix={<LockOutlined />}
-          />
-        </Form.Item>
-        <Form.Item  {...tailLayout}>
-          <Button size="large" type="primary" htmlType="submit">
-            <FormattedMessage id="modify.submit" />
-          </Button>
-        </Form.Item>
-      </Form>
+      <Card bordered={false}>
+        <Form form={form} onFinish={values => handleSubmit(values as ModifyData)} style={{ marginTop: 40 }} {...layout} >
+          <Form.Item
+            name="oldPwd"
+            label={formatMessage({ id: 'modify.oldPwd.label' })}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'modify.oldPwd.role-required' }),
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder={formatMessage({ id: 'modify.oldPwd.placeholder' })}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <Form.Item
+            name="newPwd"
+            label={formatMessage({ id: 'modify.newPwd.label' })}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'modify.newPwd.role-required' }),
+              },
+              {
+                pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,32}$/,
+                message: formatMessage({ id: 'modify.newPwd.role-pattern' }),
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder={formatMessage({ id: 'modify.newPwd.placeholder' })}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <Form.Item
+            name="checkPwd"
+            label={formatMessage({ id: 'modify.checkPwd.label' })}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'modify.checkPwd.role-required' }),
+              },
+              {
+                validator: (_, value) => (value === '' || value === form.getFieldValue('newPwd')) ?
+                  Promise.resolve() : Promise.reject(formatMessage({ id: 'modify.checkPwd.role-validator' })),
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder={formatMessage({ id: 'modify.checkPwd.placeholder' })}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <Form.Item  {...tailLayout}>
+            <Button size="large" type="primary" htmlType="submit">
+              <FormattedMessage id="modify.submit" />
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </PageHeaderWrapper>
   )
 };
--- src/pages/resetPwd/index.tsx
+++ src/pages/resetPwd/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Form, Input, Button, notification } from 'antd';
+import { Card, Form, Input, Button, notification } from 'antd';
 import { LockOutlined } from '@ant-design/icons';
 import { FormattedMessage, formatMessage } from 'umi-plugin-react/locale';
 import { ResetData } from './data';
@@ -8,13 +8,14 @@ import { resetPwd } from './service';
 
 const layout = {
   labelCol: { span: 8 },
-  wrapperCol: { span: 16 },
+  wrapperCol: { span: 8 },
 };
 const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
 const ResetView: React.FC<{}> = () => {
+  const [form] = Form.useForm();
 
   const handleSubmit = async (values: ResetData) => {
     try {
@@ -31,44 +32,54 @@ const ResetView: React.FC<{}> = () => {
   };
 
   return (
-    <PageHeaderWrapper title={formatMessage({ id: 'reset.title' })}>
-      <Form {...layout} onFinish={values => handleSubmit(values as ResetData)}>
-        <Form.Item
-          name="newPwd"
-          label={formatMessage({ id: 'reset.newPwd.label' })}
-          rules={[
-            {
-              required: true,
-              message: formatMessage({ id: 'reset.newPwd.role-required' }),
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder={formatMessage({ id: 'reset.newPwd.placeholder' })}
-            prefix={<LockOutlined />}
-          />
-        </Form.Item>
-        <Form.Item
-          name="checkPwd"
-          label={formatMessage({ id: 'reset.checkPwd.label' })}
-          rules={[
-            {
-              required: true,
-              message: formatMessage({ id: 'reset.checkPwd.role-required' }),
-            },
-          ]}
-        >
-          <Input.Password
-            placeholder={formatMessage({ id: 'reset.checkPwd.placeholder' })}
-            prefix={<LockOutlined />}
-          />
-        </Form.Item>
-        <Form.Item  {...tailLayout}>
-          <Button size="large" type="primary" htmlType="submit">
-            <FormattedMessage id="reset.submit" />
-          </Button>
-        </Form.Item>
-      </Form>
+    <PageHeaderWrapper pageHeaderRender={() => (<></>)}>
+      <Card bordered={false} title={formatMessage({ id: 'reset.title' })} style={{ marginTop: 60 }} >
+        <Form form={form} style={{ marginTop: 40 }} {...layout} onFinish={values => handleSubmit(values as ResetData)}>
+          <Form.Item
+            name="newPwd"
+            label={formatMessage({ id: 'reset.newPwd.label' })}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'reset.newPwd.role-required' }),
+              },
+              {
+                pattern: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,32}$/,
+                message: formatMessage({ id: 'reset.newPwd.role-pattern' }),
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder={formatMessage({ id: 'reset.newPwd.placeholder' })}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <Form.Item
+            name="checkPwd"
+            label={formatMessage({ id: 'reset.checkPwd.label' })}
+            rules={[
+              {
+                required: true,
+                message: formatMessage({ id: 'reset.checkPwd.role-required' }),
+              },
+              {
+                validator: (_, value) => (value === '' || value === form.getFieldValue('newPwd')) ?
+                  Promise.resolve() : Promise.reject(formatMessage({ id: 'reset.checkPwd.role-validator' })),
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder={formatMessage({ id: 'reset.checkPwd.placeholder' })}
+              prefix={<LockOutlined />}
+            />
+          </Form.Item>
+          <Form.Item  {...tailLayout}>
+            <Button size="large" type="primary" htmlType="submit">
+              <FormattedMessage id="reset.submit" />
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </PageHeaderWrapper>
   )
 };
